6e462f2b5d9381b28025514b46c9cd2cee75a266
Sarath Peter <sarathpeterofficial@gmail.com>
2025-05-25 07:44:37 +0200

    Version2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['./index.css'],
-  modules: [
-    'motion-v/nuxt',
-  ],
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
 })
